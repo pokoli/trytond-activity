de9bef17cfecdfd01ebc32a143ad87e27dc84128
resteve <resteve@zikzakmedia.com>
2013-11-11 19:02:26 +0100

    setup add views package

--- setup.py
+++ setup.py
@@ -49,7 +49,7 @@ setup(name='%s_%s' % (PREFIX, MODULE),
         ],
     package_data={
         'trytond.modules.%s' % MODULE: (info.get('xml', [])
-            + ['tryton.cfg', 'locale/*.po', 'tests/*.rst']),
+            + ['tryton.cfg', 'locale/*.po', 'view/*.xml', 'tests/*.rst']),
         },
     classifiers=[
         'Development Status :: 5 - Production/Stable',
