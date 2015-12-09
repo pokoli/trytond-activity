afa0f6860443350598e57bf329db7e6d4619168b
Sergi Almacellas Abellana <sergi@koolpi.com>
2015-12-09 23:25:03 +0100

    Migrate to version 3.8

--- tests/test_activity.py
+++ tests/test_activity.py
@@ -1,24 +1,13 @@
-#!/usr/bin/env python
-#This file is part of Tryton.  The COPYRIGHT file at the top level of
-#this repository contains the full copyright notices and license terms.
+# The COPYRIGHT file at the top level of this repository contains the full
+# copyright notices and license terms.
 import unittest
 import trytond.tests.test_tryton
-from trytond.tests.test_tryton import test_view, test_depends
+from trytond.tests.test_tryton import ModuleTestCase
 
 
-class ActivityTestCase(unittest.TestCase):
+class ActivityTestCase(ModuleTestCase):
     'Test Activity module'
-
-    def setUp(self):
-        trytond.tests.test_tryton.install_module('activity')
-
-    def test0005views(self):
-        'Test views'
-        test_view('activity')
-
-    def test0006depends(self):
-        'Test depends'
-        test_depends()
+    module = 'activity'
 
 
 def suite():
